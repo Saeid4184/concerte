f494bd7f1b780e4492cfb734078550e6549bb5c5
Saeid4184 <96084333+Saeid4184@users.noreply.github.com>
2026-07-22 04:41:15 +0330

    Update AppDatabase.kt

--- app/src/main/java/ir/factory/entryexit/data/AppDatabase.kt
+++ app/src/main/java/ir/factory/entryexit/data/AppDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import net.zetetic.android.database.sqlcipher.SupportFactory
 
 @Database(entities = [PersonEntity::class, LogEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
